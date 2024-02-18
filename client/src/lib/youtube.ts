4f1b3e8ef6b1bf1ae155a68b1f0b110db1b76046
Kaushik Sharma <kaushik445566@gmail.com>
2024-02-18 19:24:08 +0530

    server: chat gpt integration done

--- client/src/lib/youtube.ts
+++ client/src/lib/youtube.ts
@@ -16,22 +16,22 @@ export async function searchYoutube(searchQuery: string) {
 }
 
 // get transcript from youtube video Id
-// export async function getTranscript(videoId: string) {
-//   try {
-//     let transcript_arr = await YoutubeTranscript.fetchTranscript(videoId, {
-//       lang: "en",
-//       country: "EN",
-//     });
-//     let transcript = "";
-//     for (let t of transcript_arr) {
-//       transcript += t.text + " ";
-//     }
-//     return transcript.replaceAll("\n", " ");
-//   } catch (error) {
-//     console.log(error);
-//     return "";
-//   }
-// }
+export async function getTranscript(videoId: string) {
+  try {
+    let transcript_arr = await YoutubeTranscript.fetchTranscript(videoId, {
+      lang: "en",
+      country: "EN",
+    });
+    let transcript = "";
+    for (let t of transcript_arr) {
+      transcript += t.text + " ";
+    }
+    return transcript.replaceAll("\n", " ");
+  } catch (error) {
+    console.log(error);
+    return "";
+  }
+}
 
 // GetQuestion from Transcript
 
