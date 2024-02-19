2dd067c14d1e97445b4331d30b69b585066435b1
Kaushik Sharma <kaushik445566@gmail.com>
2024-02-19 19:04:37 +0530

    client: build fix

--- client/src/components/home/hero-parallax.tsx
+++ client/src/components/home/hero-parallax.tsx
@@ -72,7 +72,7 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Master in-demand skills. <br /> Earn what you're worth.
+        Master in-demand skills. <br /> Earn what you&apos;re worth.
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         We build beautiful products with the latest technologies and frameworks. We are a team of
