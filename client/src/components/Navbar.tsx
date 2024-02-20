f42cd69ba3fb975ce062f8a9d909aec63294ff93
Kaushik Sharma <kaushik445566@gmail.com>
2024-02-20 22:47:35 +0530

    client: bug fixing

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async (props: Props) => {
   return (
     <nav className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl">
-        <Link href="/gallery" className="items-center hidden gap-2 sm:flex">
+        <Link href="/" className="items-center hidden gap-2 sm:flex">
           <p className="rounded-lg border-2 border-b-8 border-r-8 border-green-600 px-2 py-1 text-lg font-bold transition-all hover:-translate-y-[2px] md:block dark:border-green-600">
             <span className="text-green-600 mr-[1px]">skill</span>Skulpt
           </p>
