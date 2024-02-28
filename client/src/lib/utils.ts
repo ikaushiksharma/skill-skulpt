5a4f6133f5f10eabed8378722e6504f22410dfc3
Kaushik Sharma <kaushik445566@gmail.com>
2024-02-28 23:44:09 +0530

    server: mail template update

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -6,76 +6,43 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const htmlTemplate = (email: string, userName: string, courseName: string) => `
-<html>
-    <head>
-        <style type='text/css'>
-            body, html {
-                margin: 0;
-                padding: 0;
-            }
-            body {
-                color: #e4e4e7;
-                display: table;
-                font-family: Georgia, serif;
-                font-size: 24px;
-                text-align: center;
-            }
-            .container {
-                background:#1f2937;
-                border: 20px solid #a855f7;
-                width: 750px;
-                height: 550px;
-                display: table-cell;
-                vertical-align: middle;
-            }
-            .logo {
-                color: #a855f7;
-            }
+<div style=" color: #e4e4e7;
+display: table;
+font-family: Georgia, serif;
+font-size: 24px;
+text-align: center;">
+<div style="background:#1f2937;
+border: 20px solid #a855f7;
+width: 750px;
+height: 550px;
+display: table-cell;
+vertical-align: middle;">
+<div style="color: #a855f7;">
+Skill Skulpt
+</div>
 
-            .marquee {
-                color: #a855f7;
-                font-size: 48px;
-                margin: 20px;
-            }
-            .assignment {
-                margin: 20px;
-            }
-            .person {
-                border-bottom: 2px solid white;
-                font-size: 32px;
-                font-style: italic;
-                margin: 20px auto;
-                width: 400px;
-            }
-            .reason {
-                margin: 20px;
-            }
-        
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="logo">
-                Skill Skulpt
-            </div>
+<div style="color: #a855f7;
+font-size: 48px;
+margin: 20px;">
+Certificate of Completion
+</div>
 
-            <div class="marquee">
-                Certificate of Completion
-            </div>
+<div style="margin: 20px;">
+This certificate is presented to
+</div>
 
-            <div class="assignment">
-                This certificate is presented to
-            </div>
+<div style="                border-bottom: 2px solid white;
+font-size: 32px;
+font-style: italic;
+margin: 20px auto;
+width: 400px;">
+${userName}
+</div>
 
-            <div class="person">
-                ${userName}
-            </div>
-
-            <div class="reason">
-                For completing the course on<br/>
-                ${courseName}
-            </div>
-        </div>
-    </body>
-</html>
+<div style="margin: 20px;">
+For completing the course on<br/>
+${courseName}
+</div>
+</div>
+</div>
 `;
